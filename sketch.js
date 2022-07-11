b760dd57bc0144e46b5a8e72cb5472b5654e81d1
Andreas Ehrlund <andreas.ehrlund@sectra.com>
2022-07-11 14:56:38 +0200

    better project setup

--- sketch.js
+++ sketch.js
@@ -37,6 +37,7 @@ function setup() {
 			}
 			if (y == 0 || y == rows) {
 				if( x != 0 && x != cols) {
+          // only wiggle edge points along the edge
 					points[x][y][0] += random(-wiggleFactor,wiggleFactor);
 			}}
 			else {
@@ -44,7 +45,6 @@ function setup() {
 			}
 		}
 	}
-    //TODO: wiggle edges in only 1 dimension
 }
 
 function draw() {
@@ -53,25 +53,31 @@ function draw() {
 	stroke(255);
 	strokeWeight(1);
 	
-	for (let colNo = 0; colNo <= cols; colNo++) {
-		for (let rowNo = 0; rowNo <= rows; rowNo++) {
-			currentX = points[colNo][rowNo][0];
-			currentY = points[colNo][rowNo][1];
-			if (colNo < cols)
-            {
-                line(currentX, currentY, points[colNo+1][rowNo][0],points[colNo+1][rowNo][1]);
-            }
-            
-			if (rowNo < rows) {
-				line(currentX, currentY, points[colNo][rowNo+1][0],points[colNo][rowNo+1][1]);
-            }
-			
-			if (colNo > 0 && rowNo > 0) {
-				drawColSubdivs(colNo, rowNo);
-				drawRowSubdivs(colNo, rowNo);
-			}
-			
+	//setCenter( floor(width/2), floor(height/2) );
+	//polarTriangle(30, 100, 50);
 
+	drawMainGrid();
+
+	function drawMainGrid() {
+		for (let colNo = 0; colNo <= cols; colNo++) {
+			for (let rowNo = 0; rowNo <= rows; rowNo++) {
+				currentX = points[colNo][rowNo][0];
+				currentY = points[colNo][rowNo][1];
+				if (colNo < cols) {
+					line(currentX, currentY, points[colNo + 1][rowNo][0], points[colNo + 1][rowNo][1]);
+				}
+
+				if (rowNo < rows) {
+					line(currentX, currentY, points[colNo][rowNo + 1][0], points[colNo][rowNo + 1][1]);
+				}
+
+				if (colNo > 0 && rowNo > 0) {
+					drawColSubdivs(colNo, rowNo);
+					drawRowSubdivs(colNo, rowNo);
+				}
+
+
+			}
 		}
 	}
 
@@ -121,4 +127,11 @@ function draw() {
 
 		}
 	}
+}
+
+function cartesian2Polar(x, y){
+    distance = Math.sqrt(x*x + y*y)
+    radians = Math.atan2(y,x) //This takes y first
+    polarCoor = { distance:distance, radians:radians }
+    return polarCoor
 }
