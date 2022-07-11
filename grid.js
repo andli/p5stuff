200553402f6e9b597f2df758f1d2aa134d8e2c31
Andreas Ehrlund <andreas.ehrlund@sectra.com>
2022-07-11 14:23:46 +0200

    better wiggle

--- grid.js
+++ grid.js
@@ -6,13 +6,13 @@ let subdivs;
 
 function setup() {
 	createCanvas(500, 707); // A3 paper size
-	randomSeed(13);
+	randomSeed(14);
 
-	cols = 5;
-	rows = 8;
+	cols = 9;
+	rows = 12;
 	margin = 5;
 	wiggleFactor = 20;
-	subdivs = 4;
+	subdivs = 12;
 	cellWidth = floor(width/cols - margin);
 	
 	cellHeight = floor(height/rows - margin);
@@ -25,43 +25,56 @@ function setup() {
 	}
 
 	// wiggle points
-	for (let x = 1; x < cols; x++) {
-		for (let y = 1; y < rows; y++) {
-			points[x][y][0] += random(-wiggleFactor,wiggleFactor);
-			points[x][y][1] += random(-wiggleFactor,wiggleFactor);
+	for (let x = 0; x <= cols; x++) {
+		for (let y = 0; y <= rows; y++) {
+			if (x == 0 || x == cols) {
+				if (y != 0 && y != rows) {
+					// only wiggle edge points along the edge
+					points[x][y][1] += random(-wiggleFactor,wiggleFactor);
+			}}
+			else {
+				points[x][y][0] += random(-wiggleFactor,wiggleFactor);
+			}
+			if (y == 0 || y == rows) {
+				if( x != 0 && x != cols) {
+					points[x][y][0] += random(-wiggleFactor,wiggleFactor);
+			}}
+			else {
+				points[x][y][1] += random(-wiggleFactor,wiggleFactor);
+			}
 		}
 	}
+    //TODO: wiggle edges in only 1 dimension
 }
 
 function draw() {
-	background(0);
+	noLoop();
+    background(0);
 	stroke(255);
 	strokeWeight(1);
 	
-	for (let colNo = 0; colNo < cols; colNo++) {
-		for (let rowNo = 0; rowNo < rows; rowNo++) {
+	for (let colNo = 0; colNo <= cols; colNo++) {
+		for (let rowNo = 0; rowNo <= rows; rowNo++) {
 			currentX = points[colNo][rowNo][0];
 			currentY = points[colNo][rowNo][1];
-			line(currentX, currentY, points[colNo+1][rowNo][0],points[colNo+1][rowNo][1]);
-			line(currentX, currentY, points[colNo][rowNo+1][0],points[colNo][rowNo+1][1]);
-
-			// if we are the last row, draw one final edge to complete the grid
-			if (colNo == cols-1) {
-				line(points[colNo+1][rowNo][0], points[colNo+1][rowNo][1], points[colNo+1][rowNo+1][0], points[colNo+1][rowNo+1][1]);
-			}
-			if (rowNo == rows-1) { 
-				line(points[colNo][rowNo+1][0], points[colNo][rowNo+1][1], points[colNo+1][rowNo+1][0], points[colNo+1][rowNo+1][1]);
-			} 
-
+			if (colNo < cols)
+            {
+                line(currentX, currentY, points[colNo+1][rowNo][0],points[colNo+1][rowNo][1]);
+            }
+            
+			if (rowNo < rows) {
+				line(currentX, currentY, points[colNo][rowNo+1][0],points[colNo][rowNo+1][1]);
+            }
+			
 			if (colNo > 0 && rowNo > 0) {
-				drawSubdivs(colNo, rowNo);
+				drawRowSubdivs(colNo, rowNo);
 			}
 			
 
 		}
 	}
 
-	function drawSubdivs(colNo, rowNo) {
+	function drawRowSubdivs(colNo, rowNo) {
 		for (let subdiv = 1; subdiv < subdivs; subdiv++) {
 			lastX1 = points[colNo - 1][rowNo - 1][0];
 			lastY1 = points[colNo - 1][rowNo - 1][1];
