210b966be1622bbd5bbdc6ad5402d343da3ab40c
Andreas Ehrlund <andreas.ehrlund@sectra.com>
2022-07-11 11:31:50 +0200

    gridz

--- grid.js
+++ grid.js
@@ -1,17 +1,49 @@
+let rows;
+let cols;
+let margin;
+let points;
+
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(500, 707); // A3 paper size
+	
+	cols = 5;
+	rows = 8;
+	margin = 5;
+	cellWidth = floor(width/cols - margin);
+	
+	cellHeight = floor(height/rows - margin);
+	points = [];
+	for (let x = 0; x < cols + 1; x++) {
+		points[x] = [];
+		for (let y = 0; y < rows + 1; y++) {
+			points[x][y] = [cellWidth*x, cellHeight*y];
+		}
+	}
+
+	// wiggle points
+
 }
 
 function draw() {
-	background(220);
-	//For (var BEGIN; END; INTERVAL){
-	//DO SOMETHING }
-	for (var x = 0; x < width; x += width / 10) {
-		for (var y = 0; y < height; y += height / 5) {
-			stroke(0);
-			strokeWeight(1);
-			line(x, 0, x, height);
-			line(0, y, width, y);
+	background(0);
+	stroke(255);
+	strokeWeight(1);
+	//line(10,10,200,200);
+	
+	for (let colNo = 0; colNo < cols; colNo++) {
+		for (let rowNo = 0; rowNo < rows; rowNo++) {
+			currentX = points[colNo][rowNo][0];
+			currentY = points[colNo][rowNo][1];
+			line(currentX, currentY, points[colNo+1][rowNo][0],points[colNo+1][rowNo][1]);
+			line(currentX, currentY, points[colNo][rowNo+1][0],points[colNo][rowNo+1][1]);
+
+			// if we are the last row, draw one final edge to complete the grid
+			if (colNo == cols-1) {
+				line(points[colNo+1][rowNo][0], points[colNo+1][rowNo][1], points[colNo+1][rowNo+1][0], points[colNo+1][rowNo+1][1]);
+			}
+			if (rowNo == rows-1) { 
+				line(points[colNo][rowNo+1][0], points[colNo][rowNo+1][1], points[colNo+1][rowNo+1][0], points[colNo+1][rowNo+1][1]);
+			} 
 		}
 	}
 }
