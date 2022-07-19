af0cacd213571d0a701f8e5d1a5bffc44b132df1
Andreas Ehrlund <a.ehrlund@gmail.com>
2022-07-19 16:00:21 +0200

    noise wip

---
+++ noisybezier.js
@@ -0,0 +1,88 @@
+let rows;
+let cols;
+let margin;
+let points;
+let numLines;
+let anchorDistance;
+let noiseFactor;
+
+function setup() {
+  createCanvas(1000, 1000);
+  //createCanvas(1000, 1414, SVG);// A3 paper size
+
+  noiseSeed(37);
+  margin = 20;
+  numLines = 80;
+  anchorDistance = createVector(120, 80);
+  noiseFactor = 0.005;
+  noiseScale = 0.3;
+  anchorScale = 1.5; // how far to pull the bezier handles
+}
+
+function draw() {
+  noLoop();
+  background(255);
+  stroke(0);
+  strokeWeight(1);
+  noFill();
+  let drawControlPoints = true;
+
+  // translate(width/2,height/2);
+  rect(margin, margin, width - 2 * margin, height - 2 * margin);
+
+  let step = floor((width - 2 * margin) / numLines);
+  for (let l = -numLines; l <= numLines; l++) {
+    let noiseVal1;
+    let noiseVal2;
+    let scaleControl = ((numLines + l) / numLines) * anchorScale;
+    let ax1 = margin;
+    let ay1 = height - margin - (numLines + l) * step;
+    let ax4 = width - margin - step * -l;
+    let ay4 = height - margin;
+    let x2 = ax1 + anchorDistance.x * scaleControl;
+    let y2 = ay1 + anchorDistance.y * scaleControl;
+    let x3 = ax4 - anchorDistance.y * scaleControl;
+    let y3 = ay4 - anchorDistance.x * scaleControl;
+
+    noiseVal1 = noise(ax1 * noiseFactor, ay1 * noiseFactor);
+    noiseVal2 = -noise(ax4 * noiseFactor, ay4 * noiseFactor);
+    console.log(noiseVal1, noiseVal2);
+    x2 = x2 + x2 * noiseScale * noiseVal1;
+    y2 = y2 + y2 * noiseScale * noiseVal1;
+    x3 = x3 + x3 * noiseScale * noiseVal2;
+    y3 = y3 + y3 * noiseScale * noiseVal2;
+
+    if (l >= 0) {
+      scaleControl = ((numLines - l) / numLines) * anchorScale;
+      ax1 = l * step + margin;
+      ay1 = margin;
+      ax4 = width - margin;
+      ay4 = height - l * step - margin;
+      x2 = ax1 + anchorDistance.x * scaleControl;
+      y2 = ay1 + anchorDistance.y * scaleControl;
+      x3 = ax4 - anchorDistance.y * scaleControl;
+      y3 = ay4 - anchorDistance.x * scaleControl;
+
+      noiseVal1 = noise(ax1 * noiseFactor, ay1 * noiseFactor);
+      noiseVal2 = -noise(ax4 * noiseFactor, ay4 * noiseFactor);
+      x2 = x2 + x2 * noiseScale * noiseVal1;
+      y2 = y2 + y2 * noiseScale * noiseVal1;
+      x3 = x3 + x3 * noiseScale * noiseVal2;
+      y3 = y3 + y3 * noiseScale * noiseVal2;
+    }
+
+    if (drawControlPoints) {
+      strokeWeight(8);
+      stroke("blue");
+      point(ax1, ay1);
+      point(ax4, ay4);
+      stroke("red");
+      point(x2, y2);
+      point(x3, y3);
+      stroke(0);
+      strokeWeight(1);
+    }
+
+    bezier(ax1, ay1, x2, y2, x3, y3, ax4, ay4);
+  }
+}
