d315b94de08539b05ea003a458ed875059565822
Andreas Ehrlund <a.ehrlund@gmail.com>
2022-07-23 01:34:08 +0200

    wip

--- curveflow.js
+++ curveflow.js
@@ -13,18 +13,18 @@ function draw() {
   noLoop();
   background(255);
   for (let i = 0; i < 200; i++) {
-    var x1 = width * noise(t + 15);
-    var x2 = width * noise(t + 25);
-    var x3 = width * noise(t + 35);
-    var x4 = width * noise(t + 45);
-    var y1 = height * noise(t + 55);
-    var y2 = height * noise(t + 65);
-    var y3 = height * noise(t + 75);
-    var y4 = height * noise(t + 85);
+    var x1 = width * noise(t + 1);
+    var x2 = width * noise(t + 2);
+    var x3 = width * noise(t + 3);
+    var x4 = width * noise(t + 4);
+    var y1 = height * noise(t + 5);
+    var y2 = height * noise(t + 6);
+    var y3 = height * noise(t + 7);
+    var y4 = height * noise(t + 8);
 
     bezier(x1, y1, x2, y2, x3, y3, x4, y4);
 
-    t += 0.005;
+    t += 0.007;
   }
   // clear the background every 500 frames using mod (%) operator
   if (frameCount % 500 == 0) {
--- noisybezier-v3-wip.js
+++ noisybezier-v3-wip.js
@@ -8,67 +8,58 @@ let noiseFactor;
 
 function setup() {
   createCanvas(1000, 1000); //, SVG);
-  createCanvas(1000, 1000, SVG);
+  // createCanvas(1000, 1000, SVG);
   //createCanvas(1000, 1414, SVG);// A3 paper size
 
   noiseSeed(1);
-  margin = 10;
-  numLines = 400;
-  anchorDistance = createVector(120, 80).mult(2);
+  margin = 100;
+  numLines = 5;
+
   noiseFactor = 0.015;
   noiseScale = 230;
 }
 
 function draw() {
   noLoop();
-  // background(255);
   stroke(0);
+  background(255);
   strokeWeight(1);
   noFill();
-  let drawControlPoints = false;
+  let drawControlPoints = true;
 
   drawCurves();
-  save("noisybezier.svg");
+  // save("noisybezier.svg");
 
   function drawCurves() {
     rect(margin, margin, width - 2 * margin, height - 2 * margin);
 
     let step = (width - 2 * margin) / numLines;
+    let ad;
+    let a1;
+    let a2;
+    let c1;
+    let c2;
     for (let l = -numLines; l <= numLines; l++) {
       let scaleControl;
-      let ad;
-      let a1;
-      let a2;
-      let c1;
-      let c2;
       if (l < 0) {
-        scaleControl = (numLines + l) / (1 * numLines);
-        ad = anchorDistance.copy().mult(scaleControl);
+        anchorDistance = createVector(80, 80).mult(2);
 
-        a1 = createVector(margin, height - margin - (numLines + l) * step);
-        a2 = createVector(width - margin - step * -l, height - margin);
-        c1 = a1.copy().add(ad);
-        c2 = a2.copy().sub(ad);
+        a1 = createVector(margin + (numLines + l) * step, margin); // bottom
+        a2 = createVector(margin + (numLines + l) * step, height - margin); // left
+        ad = anchorDistance.copy().mult((numLines + l) / (1 * numLines));
+        c1 = createVector(a1.x + ad.x, a1.y + ad.y);
+        ad = anchorDistance.copy().mult((numLines + l) / (1 * numLines));
+        c2 = createVector(a2.x - ad.x, a2.y - ad.y);
 
-        addNoise(c1, scaleControl);
-        addNoise(c2, scaleControl);
-      } else if (l >= 0) {
-        scaleControl = (numLines - l) / numLines;
-        ad = anchorDistance.copy().mult(scaleControl);
-        a1 = createVector(l * step + margin, margin);
-        a2 = createVector(width - margin, height - l * step - margin);
-        c1 = a1.copy().add(ad);
-        c2 = a2.copy().sub(ad);
-
-        addNoise(c1, scaleControl);
-        addNoise(c2, scaleControl);
+        // addNoise(c1, scaleControl);
+        // addNoise(c2, scaleControl);
       }
 
       if (drawControlPoints) {
         strokeWeight(8);
-        //   stroke("blue");
-        //   point(a1.x, a1.y);
-        //   point(a2.x, a2.y);
+        stroke("blue");
+        point(a1.x, a1.y);
+        point(a2.x, a2.y);
         stroke("red");
         point(c1.x, c1.y);
         point(c2.x, c2.y);
