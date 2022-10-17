444f41b8db8a025676f8d95957f7300b9d970cfa
Andreas Ehrlund <a.ehrlund@gmail.com>
2022-10-17 16:00:53 +0200

    noisy wigglegrid

--- wigglegrid-v2-noise.js
+++ wigglegrid-v2-noise.js
@@ -1,50 +1,85 @@
+const a3scalefactor = 1.414;
+const RENDER_SVG = false;
+let randomHash;
 let rows;
 let cols;
 let points;
 let subdivs;
 
 function setup() {
-  let a3scalefactor = 1.414;
-  let canvasWidth = 500;
-  //createCanvas(1000, 1414, SVG); // A3 paper size
-  createCanvas(canvasWidth, Math.round(canvasWidth * a3scalefactor)); // A3 paper size
-  randomSeed(26);
+  let params = getURLParams();
+  if (params.hash != undefined) {
+    print(params.hash);
+    randomHash = new Random(params.hash);
+    randomizedHashString = params.hash;
+  } else randomHash = new Random();
+  seed = round(randomHash.random_dec() * 1000000000);
+  noiseSeed(seed);
+  randomSeed(seed);
 
-  COLS = 5;
-  ROWS = 6;
-  wiggleDistance = 43;
-  SUBDIVS = 32;
+  let canvasWidth = 1000;
+  if (RENDER_SVG) {
+    createCanvas(canvasWidth, Math.round(canvasWidth * a3scalefactor), SVG);
+  } else {
+    createCanvas(canvasWidth, Math.round(canvasWidth * a3scalefactor));
+  }
 
-  cellWidth = floor(width / COLS);
-  cellHeight = floor(height / ROWS);
+  MARGIN = 40;
+  COLS = 11;
+  ROWS = 17;
+  WIGGLEDISTANCE = 43;
+  SUBDIVS = 26;
+  FLATTEN_EDGES = false;
+
+  cellWidth = floor((width - 2 * MARGIN) / COLS);
+  cellHeight = floor((height - 2 * MARGIN) / ROWS);
   points = [];
 
   // main grid data
   for (let x = 0; x < COLS + 1; x++) {
     points[x] = [];
     for (let y = 0; y < ROWS + 1; y++) {
-      points[x][y] = [cellWidth * x, cellHeight * y];
+      points[x][y] = [MARGIN + cellWidth * x, MARGIN + cellHeight * y];
     }
   }
 
   // wiggle points
   for (let x = 0; x <= COLS; x++) {
     for (let y = 0; y <= ROWS; y++) {
-      if (x == 0 || x == COLS) {
-        if (y != 0 && y != ROWS) {
-          // only wiggle edge points along the edge
-          points[x][y][1] += random(-wiggleDistance, wiggleDistance);
-        }
+      if (!FLATTEN_EDGES) {
+        points[x][y][0] +=
+          (noise(points[x][y][0], points[x][y][1]) - 0.5) * 100;
+        points[x][y][1] +=
+          (noise(points[x][y][0], points[x][y][1]) - 0.5) * 100;
       } else {
-        points[x][y][0] += random(-wiggleDistance, wiggleDistance);
-      }
-      if (y == 0 || y == ROWS) {
-        if (x != 0 && x != COLS) {
-          // only wiggle edge points along the edge
-          points[x][y][0] += random(-wiggleDistance, wiggleDistance);
+        if (x == 0 || x == COLS) {
+          if (y != 0 && y != ROWS) {
+            // only wiggle edge points along the edge
+            points[x][y][1] += randomHash.random_between(
+              -WIGGLEDISTANCE,
+              WIGGLEDISTANCE
+            );
+          }
+        } else {
+          points[x][y][0] += randomHash.random_between(
+            -WIGGLEDISTANCE,
+            WIGGLEDISTANCE
+          );
         }
-      } else {
-        points[x][y][1] += random(-wiggleDistance, wiggleDistance);
+        if (y == 0 || y == ROWS) {
+          if (x != 0 && x != COLS && FLATTEN_EDGES) {
+            // only wiggle edge points along the edge
+            points[x][y][0] += randomHash.random_between(
+              -WIGGLEDISTANCE,
+              WIGGLEDISTANCE
+            );
+          }
+        } else {
+          points[x][y][1] += randomHash.random_between(
+            -WIGGLEDISTANCE,
+            WIGGLEDISTANCE
+          );
+        }
       }
     }
   }
@@ -54,11 +89,13 @@ function draw() {
   noLoop();
   background(255);
   stroke(0);
-  strokeWeight(0.8);
+  strokeWeight(1.2);
   noFill();
   drawMainGrid();
 
-  //save("out.svg");
+  if (RENDER_SVG) {
+    save("out.svg");
+  }
 
   function drawMainGrid() {
     for (let colNo = 0; colNo <= COLS; colNo++) {
@@ -139,5 +176,36 @@ function draw() {
         currentY1 + deltaCurrentY * subdiv
       );
     }
+  }
+}
+
+class Random {
+  constructor(hash) {
+    if (hash == undefined) {
+      let chars = "0123456789abcdef";
+      let result = "0x";
+      for (let i = 64; i > 0; --i)
+        result += chars[Math.floor(Math.random() * chars.length)];
+      print("random hash result: " + result);
+
+      this.seed = parseInt(result.slice(0, 16), 16);
+    } else {
+      this.seed = parseInt(hash.slice(0, 16), 16);
+    }
+  }
+  random_dec() {
+    this.seed ^= this.seed << 13;
+    this.seed ^= this.seed >> 17;
+    this.seed ^= this.seed << 5;
+    return ((this.seed < 0 ? ~this.seed + 1 : this.seed) % 1000) / 1000;
+  }
+  random_between(a, b) {
+    return a + (b - a) * this.random_dec();
+  }
+  random_int(a, b) {
+    return Math.floor(this.random_between(a, b + 1));
+  }
+  random_choice(x) {
+    return x[Math.floor(this.random_between(0, x.length * 0.99))];
   }
 }
