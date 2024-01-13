ffc6223e984daad0925264d95c2b4a0ac14cc8bb
Andreas Ehrlund <a.ehrlund@gmail.com>
2024-01-13 21:26:56 +0100

    print version

--- flowertree-v1.js
+++ flowertree-v1.js
@@ -1,10 +1,13 @@
+const A3SCALE = 1.414;
+const CANVAS_WIDTH = 1000;
+const RENDER_SVG = false;
+
 let axiom = "X";
 let sentence = axiom;
 let rules = [];
-let len = 55;
-let angle = 17;
+let len = 61;
+let angle = 18;
 let generations = 5;
-let flowerTypeCounter = 0;
 
 rules[0] = {
   a: "F",
@@ -13,8 +16,7 @@ rules[0] = {
 
 rules[1] = {
   a: "X",
-  b: "F+[XF]-F[F+X]-[XF*]",
-
+  b: "F+[XF]-F[F+XF*]-[X]",
   //b: "F+[XF]-F[+FX]-[F-[XF*]]",
   //b: "F+[XF]-F[+FX]-[XF*]",
 };
@@ -65,23 +67,24 @@ function drawPetal(petalLength, petalWidth, style) {
     vertex(0, 0);
     bezierVertex(
       petalWidth,
-      -petalLength / 3,
+      -petalLength / 2,
       petalWidth,
-      (-3 * petalLength) / 3,
+      (-2 * petalLength) / 2,
       0,
       -petalLength
     );
     bezierVertex(
       -petalWidth,
-      (-3 * petalLength) / 3,
+      (-2 * petalLength) / 2,
       -petalWidth,
-      -petalLength / 3,
+      -petalLength / 2,
       0,
       0
     );
   }
   endShape();
 }
+
 function drawEpicycleFlower(x, y, numPetals, petalLength, petalWidth, style) {
   push();
   translate(x, y);
@@ -101,10 +104,9 @@ function turtle() {
   stroke(0);
   noFill();
 
+  let flowerTypeCounter = 1;
   for (let i = 0; i < sentence.length; i++) {
     let current = sentence.charAt(i);
-    flowerTypeCounter += 1;
-
     switch (current) {
       case "F":
         line(0, 0, 0, -len);
@@ -124,24 +126,34 @@ function turtle() {
         break;
       case "*": // Flower symbol
         if (flowerTypeCounter % 3 == 0) {
-          drawEpicycleFlower(0, 0, 5, 12, 6, "round");
-        } else if (flowerTypeCounter % 1 == 0) {
+          drawEpicycleFlower(0, 0, 5, 12, 5, "round");
+        } else if (flowerTypeCounter % 2 == 0) {
           drawEpicycleFlower(0, 0, 5, 12, 6, "pointy");
         }
 
         break;
     }
+    flowerTypeCounter += 1;
   }
 }
 
 function setup() {
-  createCanvas(800, 800);
+  if (RENDER_SVG) {
+    createCanvas(CANVAS_WIDTH, Math.round(CANVAS_WIDTH * A3SCALE), SVG);
+  } else {
+    createCanvas(CANVAS_WIDTH, Math.round(CANVAS_WIDTH * A3SCALE));
+  }
+
   angle = radians(angle);
   background(255);
   for (let i = 0; i < generations; i++) {
     generate();
   }
   turtle();
+
+  if (RENDER_SVG) {
+    save("out.svg");
+  }
 }
 
 function draw() {
